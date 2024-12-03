04835569fd6d48b144e816592beb005c80fca8b4
Andrey Shinkevich <a@shinkevi.ch>
2024-12-03 10:49:07 +0300

    solve day3: part2

--- src/day3.rs
+++ src/day3.rs
@@ -1,6 +1,6 @@
 // let's do state machine directly, without regexes
 
-struct Match {
+struct OpMatch {
     count: usize, 
     op1: String,
     op2: String,
@@ -8,9 +8,9 @@ struct Match {
     op2_done: bool,
 }
 
-impl Match {
-    fn new() -> Match {
-        Match {
+impl OpMatch {
+    fn new() -> OpMatch {
+        OpMatch {
             count: 0,
             op1: String::new(),
             op2: String::new(),
@@ -68,17 +68,140 @@ impl Match {
     }
 }
 
+struct DoMatch {
+    count: usize,
+}
+
+impl DoMatch {
+    fn new() -> DoMatch {
+        DoMatch {
+            count: 0,
+        }
+    }
+
+    fn check(&mut self, sym: char) -> (bool, bool) {
+        match (sym, self.count) {
+            ('d', 0) => {
+                self.count += 1;
+                (true, false)
+            },
+            ('o', 1) => {
+                self.count += 1;
+                (true, false)
+            },
+            ('(', 2) => {
+                self.count += 1;
+                (true, false)
+            },
+            (')', 3) => {
+                self.count += 1;
+                (true, true)
+            },
+            _ => (false, false)
+        }
+    }
+}
+
+struct DontMatch {
+    count: usize,
+}
+
+impl DontMatch {
+    fn new() -> DontMatch {
+        DontMatch {
+            count: 0,
+        }
+    }
+
+    fn check(&mut self, sym: char) -> (bool, bool) {
+        match (sym, self.count) {
+            ('d', 0) => {
+                self.count += 1;
+                (true, false)
+            },
+            ('o', 1) => {
+                self.count += 1;
+                (true, false)
+            },
+            ('n', 2) => {
+                self.count += 1;
+                (true, false)
+            },
+            ('\'', 3) => {
+                self.count += 1;
+                (true, false)
+            },
+            ('t', 4) => {
+                self.count += 1;
+                (true, false)
+            },
+            ('(', 5) => {
+                self.count += 1;
+                (true, false)
+            },
+            (')', 6) => {
+                self.count += 1;
+                (true, true)
+            },
+            _ => (false, false)
+        }
+    }
+}
+
 fn part1(input: &str) -> i32 {
     let mut sum = 0;
-    let mut m = Match::new();
+    let mut m = OpMatch::new();
     for c in input.chars() {
         match m.check(c) {
             (false, _) => {
-                m = Match::new();
+                m = OpMatch::new();
             },
             (true, true) => {
                 sum += m.exec();
-                m = Match::new();
+                m = OpMatch::new();
+            },
+            _ => (),
+        };
+    }
+    sum
+}
+
+fn part2(input: &str) -> i32 {
+    let mut sum = 0;
+    let mut m = OpMatch::new();
+    let mut d = DoMatch::new();
+    let mut n = DontMatch::new();
+    let mut disabled = false;
+    for c in input.chars() {
+        match d.check(c) {
+            (false, _) => {
+                d = DoMatch::new();
+            },
+            (true, true) => {
+                disabled = false;
+                d = DoMatch::new();
+            },
+            _ => (),
+        };
+        match n.check(c) {
+            (false, _) => {
+                n = DontMatch::new();
+            },
+            (true, true) => {
+                disabled = true;
+                n = DontMatch::new();
+            },
+            _ => (),
+        };
+        match m.check(c) {
+            (false, _) => {
+                m = OpMatch::new();
+            },
+            (true, true) => {
+                if !disabled {
+                    sum += m.exec();
+                }
+                m = OpMatch::new();
             },
             _ => (),
         };
@@ -94,6 +217,12 @@ mod tests {
     fn test_part1() {
         let input = std::fs::read_to_string("input/day3.txt").unwrap();
         assert_eq!(part1(&input), 167650499);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = std::fs::read_to_string("input/day3.txt").unwrap();
+        assert_eq!(part2(&input), 95846796);
     }
 }
 
