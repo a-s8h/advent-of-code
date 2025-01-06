e020000a5b399214c0c111661f00b4ed769ff7df
Andrey Shinkevich <a@shinkevi.ch>
2025-01-06 23:34:13 +0300

    sketch running part based on proc macro

--- src/utils/runner.rs
+++ src/utils/runner.rs
@@ -2,95 +2,68 @@ use std::fmt::Display;
 use std::fs;
 use std::path::PathBuf;
 
-pub struct Runner<I, P1, P2>
-where
-    P1: FnOnce(&I) -> Box<dyn Display>,
-    P2: FnOnce(&I) -> Box<dyn Display>,
-{
-    parse_input: fn(&str) -> I,
-    part1: P1,
-    part2: Option<P2>,
+pub use aoc_macro::aoc;
+
+#[derive(Clone)]
+pub enum AocPart {
+    Parse,
+    Part1,
+    Part2,
+}
+
+inventory::collect!(AocSolution);
+
+pub struct AocSolution {
+    pub(crate) part: AocPart,
+    pub(crate) func: fn(&str) -> Box<dyn Display>,
+}
+
+pub struct Runner {
+    parse_input: fn(&str) -> Box<dyn Display>,
+    part1: fn(&Box<dyn Display>) -> Box<dyn Display>,
+    part2: Option<fn(&Box<dyn Display>) -> Box<dyn Display>>,
     day: u32,
     year: u32,
 }
 
-pub struct RunnerBuilder<I, P1, P2>
-where
-    P1: FnOnce(&I) -> Box<dyn Display>,
-    P2: FnOnce(&I) -> Box<dyn Display>,
-{
-    parse_input: Option<fn(&str) -> I>,
-    part1: Option<P1>,
-    part2: Option<P2>,
-    day: Option<u32>,
-    year: Option<u32>,
-}
+pub fn discover_and_run(year: u32, day: u32) -> Result<(), Box<dyn std::error::Error>> {
+    let solutions: Vec<&AocSolution> = inventory::iter::<AocSolution>
+        .into_iter()
+        .collect();
 
-impl<I, P1, P2> RunnerBuilder<I, P1, P2>
-where
-    P1: FnOnce(&I) -> Box<dyn Display>,
-    P2: FnOnce(&I) -> Box<dyn Display>,
-{
-    pub fn new() -> Self {
-        RunnerBuilder {
-            parse_input: None,
-            part1: None,
-            part2: None,
-            day: None,
-            year: None,
+    let mut parser = None;
+    let mut part1 = None;
+    let mut part2 = None;
+
+    for solution in solutions {
+        match solution.part {
+            AocPart::Parse => parser = Some(solution.func),
+            AocPart::Part1 => part1 = Some(solution.func),
+            AocPart::Part2 => part2 = Some(solution.func),
         }
     }
 
-    pub fn with_parser(mut self, parser: fn(&str) -> I) -> Self {
-        self.parse_input = Some(parser);
-        self
+    let parser = parser.ok_or("No parser found")?;
+    let part1 = part1.ok_or("No part1 solution found")?;
+
+    let runner = Runner {
+        parse_input: parser,
+        part1,
+        part2,
+        day,
+        year,
+    };
+
+    let (part1_result, part2_result) = runner.run()?;
+    println!("Part 1: {}", part1_result);
+    if let Some(part2) = part2_result {
+        println!("Part 2: {}", part2);
     }
 
-    pub fn with_part1(mut self, part1: P1) -> Self {
-        self.part1 = Some(part1);
-        self
-    }
-
-    pub fn with_part2(mut self, part2: P2) -> Self {
-        self.part2 = Some(part2);
-        self
-    }
-
-    pub fn with_day(mut self, day: u32) -> Self {
-        self.day = Some(day);
-        self
-    }
-
-    pub fn with_year(mut self, year: u32) -> Self {
-        self.year = Some(year);
-        self
-    }
-
-    pub fn build(self) -> Result<Runner<I, P1, P2>, &'static str> {
-        let parse_input = self.parse_input.ok_or("Parser function is required")?;
-        let part1 = self.part1.ok_or("Part 1 function is required")?;
-        let day = self.day.ok_or("Day is required")?;
-        let year = self.year.ok_or("Year is required")?;
-
-        Ok(Runner {
-            parse_input,
-            part1,
-            part2: self.part2,
-            day,
-            year,
-        })
-    }
+    Ok(())
 }
 
-impl<I, P1, P2> Runner<I, P1, P2>
-where
-    P1: FnOnce(&I) -> Box<dyn Display>,
-    P2: FnOnce(&I) -> Box<dyn Display>,
-{
-    pub fn builder() -> RunnerBuilder<I, P1, P2> {
-        RunnerBuilder::new()
-    }
-
+impl Runner {
     pub fn run(&self) -> Result<(Box<dyn Display>, Option<Box<dyn Display>>), std::io::Error> {
         let input_path = PathBuf::from("input")
             .join(format!("y{}", self.year))
@@ -99,7 +72,7 @@ where
         let input = fs::read_to_string(input_path)?;
         let parsed = (self.parse_input)(&input);
         let part1_result = (self.part1)(&parsed);
-        let part2_result = self.part2.as_ref().map(|p2| p2(&parsed));
+        let part2_result = self.part2.map(|p2| p2(&parsed));
         Ok((part1_result, part2_result))
     }
 }
